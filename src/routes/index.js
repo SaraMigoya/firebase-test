95743b8f07722ffa3e6c32a0f40a656e36ac8bb6
SaraMigoya <satumigoya@hotmail.com>
2021-12-27 16:43:38 -0300

    Add GET and POST methods

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,22 +3,35 @@ const { Router}= require('express');
 const router = Router();
 const admin = require('firebase-admin')
 require('dotenv').config()
-
+var serviceAccount = require("../../node-firebase-test-fb4db-firebase-adminsdk-4k1nh-1ed50eebfa.json");
 admin.initializeApp({
-    credential:admin.credential.applicationDefault(),
+    credential:admin.credential.cert(serviceAccount),
     databaseURL: process.env.DB_URL
 })
 
 const db = admin.database()
 
 router.get('/', (req, res) => {
-
-       res.render('index')
+    db.ref('contacts').once('value', (snapshot)=>{
+        const data = snapshot.val()
+        res.render('index', {contacts: data})
+    })
 
 })
 
 router.post('/new-contact', (req,res) =>{
     console.log(req.body)
+    
+    const newContact = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        phone:req.body.phone
+    }
+    db.ref('contacts').push(newContact)
+
+    res.send("recived")
+
 })
 
 module.exports = router;
