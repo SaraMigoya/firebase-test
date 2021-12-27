81f1171f284ed26c329d784f35efd21c411b0d46
SaraMigoya <satumigoya@hotmail.com>
2021-12-27 14:01:07 -0300

    Add firebase

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,15 @@
 
 const { Router}= require('express');
 const router = Router();
+const admin = require('firebase-admin')
+require('dotenv').config()
+
+admin.initializeApp({
+    credential:admin.credential.applicationDefault(),
+    databaseURL: process.env.DB_URL
+})
+
+const db = admin.database()
 
 router.get('/', (req, res) => {
 
@@ -8,5 +17,8 @@ router.get('/', (req, res) => {
 
 })
 
+router.post('/new-contact', (req,res) =>{
+    console.log(req.body)
+})
 
 module.exports = router;
